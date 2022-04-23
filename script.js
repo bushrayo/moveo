ba4c2b80565fa77e2e3ddaadfc5cbd66897aa128
bushrayo <bushra.younis89@gmail.com>
2022-04-24 01:07:01 +0300

    second commit

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-
-
   //channelsNumber
   const num=10;
+
   let loop=false;
 
+  //colors of rows
   const colors = new Array('#303030','#484848','#606060','#707070', '#888888', '#A0A0A0', '#B0B0B0', '#C0C0C0', '#D3D3D3');
   
+  //create table function - address row and 9 audio rows
   function createTable() {
     let table = document.getElementById("theTable");
+    //address row
     let row0 = table.insertRow(0);
     let cell0 = row0.insertCell(0);
     cell0.innerHTML = "List of channels";
     cell0.classList.add("address");
     
-    
-
+    //audio rows
     for (let index = 1; index < num; index++) {
         let rowI = table.insertRow(index);
         let cellI = rowI.insertCell(0);
         let cellII = rowI.insertCell(1);
 
-        //add audio src for each row
+        //add audio src for each row, 
         cellI.innerHTML = '<audio id="audio-player'+ index + '"controls="controls" src="./sounds/song' + index + '.mp3" type="audio/mpeg" >';
         cellII.innerHTML = '<button id="muteButton"' + index + ' onclick="enableMute(' + index + ')" type="button"> Mute </button>';
 
@@ -33,8 +34,8 @@
 
 createTable(); 
 
+//enable mute functuin
 function enableMute(index){
-
   let audioID = document.getElementById("audio-player" + index);
   if(audioID.muted == true)
     audioID.muted=false;
@@ -43,40 +44,57 @@ function enableMute(index){
 }
 
 function playAll(){
+    loop=false;
     for (let index = 1; index < num; index++) {
         let audioID = document.getElementById("audio-player" + index);
-        if(audioID.muted==false)
+        if(audioID.muted==false){
+            audioID.load();
             audioID.play();
-        
+        }
+        else
+            audioID.load();
+
+
     }
 
 }
 
 function stopAll(){
+    loop=false;
     for (let index = 1; index < num; index++) {
         let audioID = document.getElementById("audio-player" + index);
         audioID.pause();
-        audioID.load();
+        audioID.currentTime=0;
+        //audioID.load();
         
     }
 
 }
 
 async function loopAll(){
+    
+    //loop should start:
     if(loop==false){
         loop=true;
+        //set all the audio to start point time
+       
+        for (let index = 1; index < num; index++) {
+            let audioID = document.getElementById("audio-player" + index);
+            audioID.pause();
+            audioID.currentTime=0;
+      }
     }
+    //loop should stop: 
     else{
         loop=false;
         stopAll();
-    }
-    
-    
+        }
+    //if loop==true: start the while & use "awiat" function 
     let index=1;
 
-        while(loop){
-            
+        while(loop){            
             let audioID = document.getElementById("audio-player" + index);
+            audioID.currentTime=0;
             audioID.play();
             let time=audioID.duration;
             await new Promise(resolve => setTimeout(resolve, time*1000));
@@ -87,5 +105,3 @@ async function loopAll(){
            
         }
     }
-
-
